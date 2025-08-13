b213842548be7e6f345c300889216749bcdc01c4
rotomoo <quddlf410@naver.com>
2025-08-13 18:05:25 +0900

    fix : 모바일 환경 호환성 변경

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 function Layout() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen text-gray-800">
       <Header />
       <main className="flex-grow">
         <Outlet />
